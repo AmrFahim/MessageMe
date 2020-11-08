930c4977f6610ddc9565bc0d444b0511a64022b4
Amr Fahim <amrmf2020@gmail.com>
2020-11-08 17:30:42 +0200

    first commit

---
+++ app/assets/javascripts/channels/chatroom.coffee
@@ -0,0 +1,10 @@
+App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
+  connected: ->
+    # Called when the subscription is ready for use on the server
+
+  disconnected: ->
+    # Called when the subscription has been terminated by the server
+
+  received: (data) ->
+    $('#message-contianer').append data.my_message
+    scroll_bottom()
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+  def new 
+    
+  end
+
+  def create 
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id  
+      flash[:success] =" Logged in successfully"
+      redirect_to root_path   
+    else
+      flash.now[:error] = "Invalid username or password "    
+      render 'new'          
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil 
+    flash[:success] =" Logged out successfully"
+    redirect_to login_path
+  end
+
+  private 
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You already logged in "
+      redirect_to root_path
+    end
+  end
+end
